dffb0c6b64503c808cef0272dab8d65dfd5b79df
Siddharth <siddharth.mahendranv@gmail.com>
2026-04-30 19:06:34 +0200

    Confirmed values are displaying in columns

--- process.php
+++ process.php
@@ -8,11 +8,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $birthdate = $conn->real_escape_string($_POST['birthdate']);
     $adress = $conn->real_escape_string($_POST['adress']);
     $phonenumber = $conn->real_escape_string($_POST['phonenumber']);
+    $email = $conn->real_escape_string($_POST['email']); // New column
 
     if (!empty($id)) {
-        $sql = "UPDATE birthdays SET name='$name', birthdate='$birthdate', adress='$adress', phonenumber='$phonenumber' WHERE id='$id'";
+        // Update existing record
+        $sql = "UPDATE birthdays SET 
+                name='$name', 
+                birthdate='$birthdate', 
+                adress='$adress', 
+                phonenumber='$phonenumber', 
+                email='$email' 
+                WHERE id='$id'";
     } else {
-        $sql = "INSERT INTO birthdays (name, birthdate, adress, phonenumber) VALUES ('$name', '$birthdate', '$adress', '$phonenumber')";
+        // Insert new record
+        $sql = "INSERT INTO birthdays (name, birthdate, adress, phonenumber, email) 
+                VALUES ('$name', '$birthdate', '$adress', '$phonenumber', '$email')";
     }
 
     $conn->query($sql);
